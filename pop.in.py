7d2894c0e40877c604c79bbfd8e9ee2edf222d56
Surajit <Surajit092@gm.com>
2024-06-03 00:15:50 +0530

    add pop/PYTHON

---
+++ pop.in.py
@@ -0,0 +1,8 @@
+list1=["mango","litchi","guava","banana"]
+print(list1)
+print(list1.pop())  ## only pop().. print last item
+
+print(list1)
+
+print(list1.pop(0)) ##print first item
+print(list1)        ## print rest of item
